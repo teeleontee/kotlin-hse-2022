8b6e2d13fdcddbde58de63965d4b2583ba170f94
teeleontee <leontyevts@gmail.com>
2023-02-01 14:34:09 +0300

    tailrec fixed and gets

--- homework02/lib/src/main/kotlin/binomial/FList.kt
+++ homework02/lib/src/main/kotlin/binomial/FList.kt
@@ -1,6 +1,6 @@
 package binomial
 
-import java.util.InputMismatchException
+import java.util.NoSuchElementException
 
 /*
  * FList - реализация функционального списка
@@ -27,7 +27,7 @@ sealed class FList<T>: Iterable<T> {
     internal class FListIterator<T>(var flist : FList<T>) : Iterator<T> {
         override fun hasNext(): Boolean = !flist.isEmpty
 
-        override fun next(): T = if (!hasNext()) throw InputMismatchException("Empty") else {
+        override fun next(): T = if (!hasNext()) throw NoSuchElementException("Empty") else {
             val ans = flist.head
             flist = flist.tail
             ans!!
@@ -77,14 +77,14 @@ sealed class FList<T>: Iterable<T> {
     override fun iterator(): Iterator<T> = FListIterator(this)
 
     data class Nil<T>(private val dummy: Int=0) : FList<T>() {
-        override val head: T?
-            get() = null
+        override val head: T? = null
+
         override val tail: FList<T>
             get() = nil()
 
         override val size: Int = 0
-        override val isEmpty: Boolean
-            get() = true
+
+        override val isEmpty: Boolean = true
 
         override fun <U> fold(base: U, f: (U, T) -> U): U = base
 
@@ -94,14 +94,44 @@ sealed class FList<T>: Iterable<T> {
     }
 
     data class Cons<T>(override val head: T, override val tail: FList<T>) : FList<T>() {
-        override val size: Int
-            get() = tail.size + 1
-        override val isEmpty: Boolean
-            get() = false
+        override val size: Int = tail.size + 1
+        override val isEmpty: Boolean = false
 
-        override fun <U> fold(base: U, f: (U, T) -> U): U = tail.fold(f(base, head), f)
-        override fun filter(f: (T) -> Boolean): FList<T> = if (f(head)) Cons(head, tail.filter(f)) else tail.filter(f)
-        override fun <U> map(f: (T) -> U): FList<U> = Cons(f(head), tail.map(f))
+        override fun <U> fold(base: U, f: (U, T) -> U): U {
+            tailrec fun <U> folder(base: U, f: (U, T) -> U, it: FListIterator<T>): U {
+                return if (!it.hasNext())
+                    base
+                else
+                    folder(f(base, it.next()), f, it)
+            }
+            return folder(base, f, FListIterator(this))
+        }
+        override fun filter(f: (T) -> Boolean): FList<T> {
+            tailrec fun filterThrough(f: (T) -> Boolean, cur: FList<T>, it: FListIterator<T>): FList<T> {
+                return if (!it.hasNext())
+                    cur
+                else {
+                    val head = it.next()
+                    if (f(head))
+                        filterThrough(f, Cons(head, cur), it)
+                    else {
+                        filterThrough(f, cur, it)
+                    }
+                }
+            }
+            return filterThrough(f, nil(), FListIterator(this))
+        }
+        override fun <U> map(f: (T) -> U): FList<U> {
+            tailrec fun mapper(f: (T) -> U, cur: FList<U>, it: FListIterator<T>): FList<U> {
+                return if (!it.hasNext())
+                    cur
+                else {
+                    val head = it.next()
+                    mapper(f, Cons(f(head), cur), it)
+                }
+            }
+            return mapper(f, nil(), FListIterator(reverse()))
+        }
     }
 
     companion object {
@@ -110,7 +140,13 @@ sealed class FList<T>: Iterable<T> {
     }
 }
 
-internal fun <T> helpFlistOf(index: Int, vararg list: T): FList<T> = if (index == list.size) FList.nil() else FList.Cons(list[index], helpFlistOf(index + 1, *list))
+internal tailrec fun <T> helpFlistOf(index: Int, cur: FList<T>, vararg list: T): FList<T> {
+    return if (index == list.size) {
+        cur
+    } else {
+        helpFlistOf(index + 1, FList.Cons(list[index], cur), *list)
+    }
+}
 // конструирование функционального списка в порядке следования элементов
 // требуемая сложность - O(n)
-fun <T> flistOf(vararg values: T): FList<T> = helpFlistOf(0, *values)
+fun <T> flistOf(vararg values: T): FList<T> = helpFlistOf(0, FList.Nil<T>(), *values)
