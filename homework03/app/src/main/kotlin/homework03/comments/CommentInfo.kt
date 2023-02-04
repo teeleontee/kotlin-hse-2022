6561b7096cc06b484a79eb782fc6731f60819a26
T.Leontev <84567804+teeleontee@users.noreply.github.com>
2023-02-04 19:57:31 +0300

    Create CommentInfo.kt

---
+++ homework03/app/src/main/kotlin/homework03/comments/CommentInfo.kt
@@ -0,0 +1,17 @@
+package homework03.comments
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+internal data class CommentInfo(@JsonProperty("data") val data: CommentInfoData) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    internal data class CommentInfoData(
+        @JsonProperty("id") val id: String,
+        @JsonProperty("author") val author: String?,
+        @JsonProperty("created") val created: Long,
+        @JsonProperty("ups") val upvote: Int,
+        @JsonProperty("downs") val downvote: Int,
+        @JsonProperty("body") val text: String?
+    )
+}
